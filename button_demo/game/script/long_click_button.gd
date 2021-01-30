5e911544e6625c3c99de1346122308f8034e6c3f
vmjcv666 <vmjcv666@gmail.com>
2021-01-30 12:06:25 +0800

    add

--- button_demo/game/script/long_click_button.gd
+++ button_demo/game/script/long_click_button.gd
@@ -8,9 +8,9 @@ onready var canva_material = CANVA_MATERIAL_SCENE.duplicate(true)
 signal long_pressed
 signal long_released
 
-var __signal_sent = false
-var __current_duration = 0
-var __button_pressed = false
+var _signal_sent = false
+var _current_duration = 0
+var _button_pressed = false
 
 
 
@@ -25,28 +25,28 @@ func _ready():
 func _process(delta):
 	if is_disabled():
 		return
-	if __button_pressed:
-		__current_duration = lerp(__current_duration, click_duration_threshold, 0.1)
+	if _button_pressed:
+		_current_duration = lerp(_current_duration, click_duration_threshold, 0.1)
 		_update_button()
-	elif __current_duration > 0.001:
-		if __signal_sent:
+	elif _current_duration > 0.001:
+		if _signal_sent:
 			emit_signal("long_released")
-		__signal_sent = false
-		__current_duration = lerp(__current_duration, 0.0, 0.05)
+		_signal_sent = false
+		_current_duration = lerp(_current_duration, 0.0, 0.05)
 		_update_button()
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		__button_pressed = event.pressed
+		_button_pressed = event.pressed
 		accept_event()
 		get_tree().set_input_as_handled()
 
 func set_disabled(value):
 	.set_disabled(value)
 	if value:
-		__button_pressed = false
-		__current_duration = 0
-		__signal_sent = false
+		_button_pressed = false
+		_current_duration = 0
+		_signal_sent = false
 		_update_button()
 
 func nearest_po2_decimal(value,min_value,max_value):
@@ -57,9 +57,9 @@ func nearest_po2_decimal(value,min_value,max_value):
 	return ceil(value*pow_16_value)/pow_16_value
 
 func _update_button():
-	if abs(__current_duration - click_duration_threshold) < 1.0 and not __signal_sent:
-		__signal_sent = true
+	if abs(_current_duration - click_duration_threshold) < 1.0 and not _signal_sent:
+		_signal_sent = true
 		emit_signal("long_pressed")
 
-	var value = 1.0 - nearest_po2_decimal(__current_duration / click_duration_threshold, 0.0, 1.0)
+	var value = 1.0 - nearest_po2_decimal(_current_duration / click_duration_threshold, 0.0, 1.0)
 	canva_material.set_shader_param("loading", value)
