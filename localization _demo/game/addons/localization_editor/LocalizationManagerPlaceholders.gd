d0cff31b6b6275c1b4b40901941df0c144ebc8d9
majinchuan <vmjcv666@gmail.com>
2021-02-07 10:30:06 +0800

    add

---
+++ localization _demo/game/addons/localization_editor/LocalizationManagerPlaceholders.gd
@@ -0,0 +1,12 @@
+# Placeholders for LocalizationManger to use in source code: MIT License
+# @author Vladimir Petrenko
+tool
+class_name LocalizationManagerPlaceholders
+
+const NAME = "{{NAME}}"
+const OLD = "{{OLD}}"
+
+const PLACEHOLDERS = [
+ "NAME",
+ "OLD"
+]
