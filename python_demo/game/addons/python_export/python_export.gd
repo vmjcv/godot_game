8ae21efec8b28709a7d033ace92e52da5b9f3be0
vmjcv666 <vmjcv666@gmail.com>
2021-02-25 04:45:38 +0800

    add

--- python_demo/game/addons/python_export/python_export.gd
+++ python_demo/game/addons/python_export/python_export.gd
@@ -1,9 +1,11 @@
 tool
 extends EditorPlugin
 
+
 class PythonExportPlugin:
 	extends EditorExportPlugin
-
+	var exclude_list = ["res://addons/python_export"]
+	var end_exclude_list = ["py"]
 	func scan(path:String) -> Array:
 		var file_name := ""
 		var files := []
@@ -16,15 +18,26 @@ class PythonExportPlugin:
 			while file_name!="":
 				if dir.current_is_dir():
 					var sub_path = path+"/"+file_name
-					files += scan(sub_path)
+					if sub_path in exclude_list:
+						pass
+					else:
+						files += scan(sub_path)
 				else:
-					var name := path+"/"+file_name
-					files.push_back(name)
+					if file_name.get_extension() in end_exclude_list:
+						pass
+					else:
+						var name := path+"/"+file_name
+						files.push_back(name)
 				file_name = dir.get_next()
 			dir.list_dir_end()
 		return files
 
 	func _add_python_addons() -> void:
+#		var compileall_py_instance = load("res://addons/python_export/compileall_py.tscn").instance()
+#		var path_root = ProjectSettings.globalize_path("res://")
+#		compileall_py_instance.path_root = path_root
+#		compileall_py_instance._process()
+		OS.execute(ProjectSettings.globalize_path("res://addons/pythonscript/windows-64/python.exe"),["-m", "compileall", "-b",ProjectSettings.globalize_path("res://")])
 		var files = scan('res://addons/pythonscript/windows-64')
 		for python_file in files:
 			var file = File.new()
@@ -32,6 +45,18 @@ class PythonExportPlugin:
 			var buff = file.get_buffer(file.get_len())
 			file.close()
 			add_file(python_file,buff,false)
+			
+		files = scan('res://')
+		for pyc_file in files:
+			print(pyc_file.get_extension())
+			if pyc_file.get_extension() == "pyc":
+				print(pyc_file)
+				var file = File.new()
+				file.open(pyc_file, File.READ)
+				var buff = file.get_buffer(file.get_len())
+				file.close()
+				add_file(pyc_file,buff,false)
+		
 
 	func _process_hooks(hooks_path: String, args: Array) -> void:
 		var dir = Directory.new()
@@ -62,6 +87,11 @@ class PythonExportPlugin:
 	func _export_end() -> void:
 		_process_end_hooks()
 		pass
+		
+	func _export_file(path, type, features)->void:
+		if path.get_extension() in end_exclude_list:
+			skip()
+		
 
 func _init():
 	add_export_plugin(PythonExportPlugin.new())
